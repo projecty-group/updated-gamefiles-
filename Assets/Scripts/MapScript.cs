67a3062222804d1c8f585188f7b478d63bc22561
Xyeila <syelasandy@gmail.com>
2023-03-14 11:57:30 -0400

    Clickable Map Interface

---
+++ Assets/Scripts/MapScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapScript : MonoBehaviour
+{
+   public void GoToScene(string sceneName){
+
+        SceneManager.LoadScene(sceneName);
+   }
+
+  
+}
